67ce00c88f02d363ce471aa48f0fa99909498d31
ronyepeterson <ronpec@gmail.com>
2023-10-18 19:05:03 -0300

    nova conexão

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-define('HOST', '127.0.0.1');
-define('USUARIO', 'root');
-define('SENHA', '');
-define('DB', 'expoceep2023');
+define('HOST', 'https://auth-db1074.hstgr.io/');
+define('USUARIO', 'u224722929_expoceep');
+define('SENHA', 's*4*4r!G');
+define('DB', 'u224722929_expoceep');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
 ?>
