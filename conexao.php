b4b99497c68531cb9320d4b94ff9b93ee170c903
ronyepeterson <ronpec@gmail.com>
2023-10-18 19:11:38 -0300

    novo teste

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-define('HOST', 'https://auth-db1074.hstgr.io/');
+define('HOST', 'https://auth-db1074.hstgr.io');
 define('USUARIO', 'u224722929_expoceep');
 define('SENHA', 's*4*4r!G');
 define('DB', 'u224722929_expoceep');
 
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar com o banco');
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar com o banco de dados');
 ?>
