eae43e0dcac2e66e74a2458ea8ff4fdd328cc233
ronyepeterson <ronpec@gmail.com>
2023-10-18 19:08:35 -0300

    teste conexão

--- conexao.php
+++ conexao.php
@@ -4,5 +4,5 @@ define('USUARIO', 'u224722929_expoceep');
 define('SENHA', 's*4*4r!G');
 define('DB', 'u224722929_expoceep');
 
-$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
+$conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar com o banco');
 ?>
