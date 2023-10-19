84c9ccebf26984a5aeda55798a773bde003917ca
ronyepeterson <ronpec@gmail.com>
2023-10-18 21:21:16 -0300

    Voltando alterações de conexão

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
 define('HOST', 'localhost');
-define('USUARIO', 'root');
-define('SENHA', '');
-define('DB', 'expoceep2023');
+define('USUARIO', 'u224722929_expoceep');
+define('SENHA', 's*4*4r!G');
+define('DB', 'u224722929_expoceep');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar com o banco de dados..');
 ?>
